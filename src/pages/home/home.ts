3539b1e95883a827e6abccc596273c447b6f0436
Marcelo Mazon <marcelo.mazon@betha.com.br>
2018-09-25 17:20:47 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController, 
+    public alertCtrl: AlertController) {
+  }
 
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Olá Mundo!',
+      subTitle: 'Lorem ipsum dolor et mascadas helous mundus!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
